e57b4a922350aa6fb155641dc354a66f9c1b9e53
zepf-uplb <zepf.uplb@gmail.com>
2015-09-10 13:44:02 +0800

    added test cases for insertion

--- controllers/student.js
+++ controllers/student.js
@@ -5,6 +5,7 @@
 var db = require(__dirname+'/../lib/mysql'); 
 
 exports.find = function(req,res,next){
+	console.log(req.ip+" find()");	
 	db.query("SELECT * FROM student",
 	function(err,rows){
 	if(err) return next(err);
@@ -21,6 +22,7 @@ exports.insert = function(req,res,next){
 };
 
 exports.findOne = function(req,res,next){
+	console.log(req.ip+" findOne()");
 	db.query("SELECT * FROM student WHERE id=?",[req.params.id],
 	function(err,rows){
 	if(err) return next(err);
